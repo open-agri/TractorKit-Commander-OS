093226b48bace6cca9a3e00d8f61fbf2a7f17c2d
Riccardo Persello <riccardo.persello+i@icloud.com>
2020-09-13 15:54:10 +0200

    Documentation and boot message update

--- main/main.c
+++ main/main.c
@@ -1,13 +1,13 @@
-/* LVGL Example project
+/**
+ * @file main.c
+ * @author Riccardo Persello (riccardo.persello@icloud.com)
+ * @brief Main.
+ * @version 0.1
+ * @date 2020-09-11
  * 
- * Basic project to test LVGL on ESP32 based projects.
- *
- * This example code is in the Public Domain (or CC0 licensed, at your option.)
- *
- * Unless required by applicable law or agreed to in writing, this
- * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied.
+ * 
  */
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,16 +19,26 @@
 #include "freertos/semphr.h"
 #include "esp_system.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_ota_ops.h"
 
 #include "tkos.h"
+
+#define TAG "main"
 
 /**********************
  *   APPLICATION MAIN
  **********************/
 void app_main() {
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+
+    esp_app_desc_t *desc = esp_ota_get_app_description();
+
+    ESP_LOGI(TAG, "Commander boot. %s %s for ESP32 (compiled on %s %s) with ESP-IDF %s.", desc->project_name, desc->version, desc->date, desc->time, desc->idf_ver);
     
     /* If you want to use a task to create the graphic, you NEED to create a Pinned task
      * Otherwise there can be problem such as memory corruption and so on.
      * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
-    xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
+    xTaskCreatePinnedToCore(guiTask, "main", 4096*10, NULL, 0, NULL, 1);
 }
